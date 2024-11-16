15b5cf0e6e8c48af1e7ccf6efce1cc062ebbaa49
João Vitor Lima Mergulhão <jvitormergulho@gmail.com>
2024-11-16 20:56:16 -0300

    add sugested works

--- capes/src/components/ResearchGroup.js
+++ capes/src/components/ResearchGroup.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import "../style/ResearchGroup.css";
 import commentIcon from "../assets/commenticon.png";
+import heartIcon from "../assets/hearticon.png";
+import SugestedWorks from "./SugestedWorks";
 
 const ResearchGroup = () => {
   const location = useLocation();
@@ -16,7 +18,7 @@ const ResearchGroup = () => {
   const persons = [
     { id: 1, name: "Pedro Montes", image: "https://picsum.photos/id/823/400" },
     { id: 2, name: "Caroleta Costa", image: "https://picsum.photos/id/823/400" },
-    { id: 3, name: "João Mergulhão", image: "https://picsum.photos/id/823/400" },
+    { id: 3, name: "João Lima", image: "https://picsum.photos/id/823/400" },
     { id: 4, name: "Rodrigo Pereira", image: "https://picsum.photos/id/823/400" },
   ];
 
@@ -25,7 +27,7 @@ const ResearchGroup = () => {
       id: 1,
       title: "6G e IoT: Conectando um Mundo Inteligente",
       mainComment:
-      {person: persons[0], content: "O 6G promete integrar dispositivos inteligentes em larga escala, revolucionando cidades, saúde e indústrias com maior eficiência e baixa latência."},
+        { person: persons[0], content: "O 6G promete integrar dispositivos inteligentes em larga escala, revolucionando cidades, saúde e indústrias com maior eficiência e baixa latência." },
       comments: [
         { person: persons[0], content: "Ótimo artigo" },
         { person: persons[3], content: "Observações interessantes" },
@@ -35,7 +37,7 @@ const ResearchGroup = () => {
     {
       id: 2,
       title: "Reciclagem de E-lixo: Um Desafio Urbano",
-      mainComment: {person: persons[0], content: "Muito bom, pessoal!"},
+      mainComment: { person: persons[3], content: "Muito bom, pessoal!" },
       comments: [
         { person: persons[0], content: "Importante reflexão sobre o impacto ambiental" },
         { person: persons[2], content: "Gostei do enfoque nas soluções tecnológicas" },
@@ -44,7 +46,7 @@ const ResearchGroup = () => {
     {
       id: 3,
       title: "Redes 6G: O Futuro da Conectividade",
-      mainComment:  {person: persons[0], content: "Muito bom, pessoal!"},
+      mainComment: { person: persons[2], content: "Muito bom, pessoal!" },
       comments: [
         { person: persons[1], content: "Análise abrangente do futuro da conectividade" },
         { person: persons[2], content: "Perspectiva interessante sobre os desafios" },
@@ -77,8 +79,10 @@ const ResearchGroup = () => {
       <hr style={{ margin: "16px 0", border: "none", borderTop: "1px solid #ccc" }} />
 
       <div>
-        <h1>{title}</h1>
-        <h1>{description}</h1>
+        <div style={{ borderLeft: "8px solid #01CD9A", paddingLeft: "10px" }}>
+          <h1>{title}</h1>
+          <h1>{description}</h1>
+        </div>
 
         <div className="tags-container">
           {persons.map((person) => (
@@ -90,31 +94,52 @@ const ResearchGroup = () => {
         </div>
       </div>
 
+      <SugestedWorks />
+
+      <h2>Salvos pelo Grupo</h2>
       <div className="saved-works-container">
         {works.map((work) => (
           <div key={work.id} className="saved-work">
-            <h3>{work.title}</h3>
 
             <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%" }}>
-              <p style={{ marginRight: 'auto' }}>{work.mainComment.content}</p>
-              
 
-                <div>
+              <div>
+                <h3>{work.title}</h3>
+                <p style={{ marginRight: 'auto' }}>{work.mainComment.content}</p>
+              </div>
 
-                  <div key={work.mainComment.person.id} className="tag" style={{ backgroundColor: colors[work.mainComment.person.id % 4], display: 'flex', alignItems: 'center' }}>
-                    <p style={{ marginRight: '10px' }}>{work.mainComment.person.name}</p>
-                    <br-avatar image={work.mainComment.person.image}></br-avatar>
-                  </div>
 
-                  <div className="buttons">
+              <div style={{ alignItems: "center", display: "flex", flexDirection: "column" }}>
+
+                <div key={work.mainComment.person.id} className="tag" style={{ backgroundColor: colors[work.mainComment.person.id % 4], display: 'flex', alignItems: 'center' }}>
+                  <p style={{ marginRight: '10px' }}>{work.mainComment.person.name}</p>
+                  <br-avatar image={work.mainComment.person.image}></br-avatar>
+                </div>
+
+                <div className="buttons" style={{ gap: "16px" }}>
+
+
+                  <div className = "iconAndCount">
+                    <p>{work.comments.length}</p>
                     <img
                       src={commentIcon}
                       alt="Icone de comentário"
                       onClick={() => handleCommentClick(work.id)}
-                      style={{ cursor: "pointer" }}
+                      style={{ cursor: "pointer", height: "18px" }}
                     />
                   </div>
+
+                  <div className = "iconAndCount">
+                    <p>{work.comments.length}</p>
+                    <img
+                      src={heartIcon}
+                      alt="Icone de coração"
+                      style={{ cursor: "pointer", width: "22px", height: "22px" }}
+                    />
+                  </div>
+
                 </div>
+              </div>
             </div>
 
             {selectedWorkId === work.id && (
@@ -129,25 +154,35 @@ const ResearchGroup = () => {
                     </div>
 
                     <p className="comment-content">{comment.content}</p>
+
+                    <img
+                      src={heartIcon}
+                      alt="Icone de coração"
+                      style={{ alignSelf: "flex-end", cursor: "pointer", width: "22px", height: "22px" }}
+                    />
                   </div>
 
                 ))}
 
-                <input
-                  label="Adicione um comentário"
-                  placeholder="Placeholder"
-                  density="large"
-                  value={newComment}
-                  onChange={(e) => setNewComment(e.target.value)} // Atualiza o estado com o valor digitado
-                  onBlur={() => console.log('Campo perdeu foco')} // Caso precise capturar o evento de foco
-                />
+                <div className="comment-input-and-button">
+                  <p>Adicionar Comentário</p>
+                  <textarea
+                    className="comment-input"
+                    label="Adicione um comentário"
+                    placeholder="Comente aqui..."
+                    density="large"
+                    value={newComment}
+                    onChange={(e) => setNewComment(e.target.value)}
+                    onBlur={() => console.log('Campo perdeu foco')}
+                  />
 
-                <br-button
-                  icon
-                  label="Comentar"
-                  type="primary"
-                  onClick={() => addComment(work.id)}
-                ></br-button>
+                  <br-button style={{ alignSelf: "flex-end", marginTop: "16px" }}
+                    label="Comentar"
+                    type="primary"
+                    onClick={() => addComment(work.id)}
+                  ></br-button>
+
+                </div>
 
               </div>
             )}
@@ -155,6 +190,7 @@ const ResearchGroup = () => {
           </div>
         ))}
       </div>
+
     </div>
   );
 };
