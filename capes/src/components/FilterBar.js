3abacafeef85772d6eb9e47f0d6dd13a57380635
PedroMonte <pedrovitor23.monte@gmail.com>
2024-11-17 13:28:21 -0300

    feat: add filter publication date and filter button

--- capes/src/components/FilterBar.js
+++ capes/src/components/FilterBar.js
@@ -2,178 +2,190 @@ import React, { useState, useEffect } from "react";
 import "@govbr-ds/webcomponents/dist/webcomponents.umd.min.js";
 
 function FilterBar({
-  worksData,
-  applyFilters,
-  searchPerformed,
-  isShowingFilters,
+    worksData,
+    applyFilters,
+    searchPerformed,
+    isShowingFilters,
 }) {
-  const [availableTypes, setAvailableTypes] = useState({});
-  const [availableEditors, setAvailableEditors] = useState({});
-  const [availableLanguages, setAvailableLanguages] = useState({});
-  const [availableAreas, setAvailableAreas] = useState({});
-  const [oldestDate, setOldestDate] = useState(null);
-  const [newestDate, setNewestDate] = useState(null);
-  const [OACount, setOA] = useState(0);
-  const [noOACount, setNoOA] = useState(0);
+    const [availableTypes, setAvailableTypes] = useState({});
+    const [availableEditors, setAvailableEditors] = useState({});
+    const [availableLanguages, setAvailableLanguages] = useState({});
+    const [availableAreas, setAvailableAreas] = useState({});
+    const [oldestDate, setOldestDate] = useState(null);
+    const [newestDate, setNewestDate] = useState(null);
+    const [OACount, setOA] = useState(0);
+    const [noOACount, setNoOA] = useState(0);
 
-  const [openAccessOnly, setOpenAccessOnly] = useState([""]);
-  const [selectedTypes, setSelectedTypes] = useState([]);
-  const [yearRange, setYearRange] = useState([2000, 2024]);
-  const [selectedAreas, setSelectedAreas] = useState([]);
-  const [selectedLanguages, setSelectedLanguages] = useState([]);
-  const [selectedEditors, setSelectedEditors] = useState([]);
+    const [openAccessOnly, setOpenAccessOnly] = useState([""]);
+    const [selectedTypes, setSelectedTypes] = useState([]);
+    const [yearRange, setYearRange] = useState([2000, 2024]);
+    const [selectedAreas, setSelectedAreas] = useState([]);
+    const [selectedLanguages, setSelectedLanguages] = useState([]);
+    const [selectedEditors, setSelectedEditors] = useState([]);
 
-  useEffect(() => {
-    if (searchPerformed) {
-      handleFilters();
-    }
-    // Atualiza os filtros quando há uma nova pesquisa
-  }, [worksData, searchPerformed]);
+    useEffect(() => {
+        if (searchPerformed) {
+            handleFilters();
+        }
+        // Atualiza os filtros quando há uma nova pesquisa
+    }, [worksData, searchPerformed]);
 
-  const handleTypeChange = (type) => {
-    setSelectedTypes((prev) =>
-      prev.includes(type) ? prev.filter((t) => t !== type) : [...prev, type]
-    );
-  };
+    const handleTypeChange = (type) => {
+        setSelectedTypes((prev) =>
+            prev.includes(type) ? prev.filter((t) => t !== type) : [...prev, type]
+        );
+    };
 
-  const handleFilters = async () => {
-    // Contagem de tipos
-    const typeCounts = {};
-    worksData.forEach((work) => {
-      if (work.type) {
-        const type = work.type;
-        typeCounts[type] = (typeCounts[type] || 0) + 1;
-      }
-    });
-    setAvailableTypes(typeCounts);
+    const handleFilters = async () => {
+        // Contagem de tipos
+        const typeCounts = {};
+        worksData.forEach((work) => {
+            if (work.type) {
+                const type = work.type;
+                typeCounts[type] = (typeCounts[type] || 0) + 1;
+            }
+        });
+        setAvailableTypes(typeCounts);
 
-    // Contagem de idiomas
-    const languageCounts = {};
-    worksData.forEach((work) => {
-      if (work.language) {
-        const language = work.language;
-        languageCounts[language] = (languageCounts[language] || 0) + 1;
-      }
-    });
-    setAvailableLanguages(languageCounts);
+        // Contagem de idiomas
+        const languageCounts = {};
+        worksData.forEach((work) => {
+            if (work.language) {
+                const language = work.language;
+                languageCounts[language] = (languageCounts[language] || 0) + 1;
+            }
+        });
+        setAvailableLanguages(languageCounts);
 
-    // Contagem de áreas
-    const areasCount = {};
-    worksData.forEach((work) => {
-      const area = work.concepts[0].display_name;
-      areasCount[area] = (areasCount[area] || 0) + 1;
-    });
-    setAvailableAreas(areasCount);
+        // Contagem de áreas
+        const areasCount = {};
+        worksData.forEach((work) => {
+            const area = work.concepts[0].display_name;
+            areasCount[area] = (areasCount[area] || 0) + 1;
+        });
+        setAvailableAreas(areasCount);
 
-    // Contagem de editores
-    const editorsCount = {};
-    worksData.forEach((work) => {
-      const editor =
-        work.authorships[0]?.institutions[0]?.display_name || "unknown";
-      editorsCount[editor] = (editorsCount[editor] || 0) + 1;
-    });
-    setAvailableEditors(editorsCount);
+        // Contagem de editores
+        const editorsCount = {};
+        worksData.forEach((work) => {
+            const editor =
+                work.authorships[0]?.institutions[0]?.display_name || "unknown";
+            editorsCount[editor] = (editorsCount[editor] || 0) + 1;
+        });
+        setAvailableEditors(editorsCount);
 
-    // Obter ano de publicação mais novo e mais antigo
-    const publicationYears = worksData
-      .map((work) => work.publication_year)
-      .filter(Boolean);
-    if (publicationYears.length > 0) {
-      setNewestDate(Math.max(...publicationYears));
-      setOldestDate(Math.min(...publicationYears));
-    }
+        // Obter ano de publicação mais novo e mais antigo
+        const publicationYears = worksData
+            .map((work) => work.publication_year)
+            .filter(Boolean);
+        if (publicationYears.length > 0) {
+            setNewestDate(Math.max(...publicationYears));
+            setOldestDate(Math.min(...publicationYears));
+        }
 
-    let isOACount = 0;
-    let notOACount = 0;
+        let isOACount = 0;
+        let notOACount = 0;
 
-    worksData.forEach((work) => {
-      const is_oa = work.is_oa;
-      if (is_oa) {
-        isOACount++;
-      } else {
-        notOACount++;
-      }
-    });
+        worksData.forEach((work) => {
+            const is_oa = work.is_oa;
+            if (is_oa) {
+                isOACount++;
+            } else {
+                notOACount++;
+            }
+        });
 
-    setNoOA(notOACount);
-    setOA(isOACount);
-  };
+        setNoOA(notOACount);
+        setOA(isOACount);
+    };
 
-  const applySelectedFilters = () => {
-    let filteredWorks = [...worksData];
+    const applySelectedFilters = () => {
+        let filteredWorks = [...worksData];
 
-    // Aplica os filtros com base nas seleções
-    if (openAccessOnly) {
-      filteredWorks = filteredWorks.filter((work) => work.is_oa);
-    }
+        // Aplica os filtros com base nas seleções
+        if (openAccessOnly) {
+            filteredWorks = filteredWorks.filter((work) => work.is_oa);
+        }
 
-    // Filtros de tipo
-    if (selectedTypes.length > 0) {
-      filteredWorks = filteredWorks.filter((work) =>
-        selectedTypes.includes(work.type)
-      );
-    }
+        // Filtros de tipo
+        if (selectedTypes.length > 0) {
+            filteredWorks = filteredWorks.filter((work) =>
+                selectedTypes.includes(work.type)
+            );
+        }
 
-    // Filtros de áreas
-    if (selectedAreas.length > 0) {
-      filteredWorks = filteredWorks.filter((work) =>
-        selectedAreas.includes(work.concepts[0]?.display_name)
-      );
-    }
-    // Chama a função para atualizar os works na SearchBar
-    applyFilters(filteredWorks);
-  };
+        // Filtros de áreas
+        if (selectedAreas.length > 0) {
+            filteredWorks = filteredWorks.filter((work) =>
+                selectedAreas.includes(work.concepts[0]?.display_name)
+            );
+        }
+        // Chama a função para atualizar os works na SearchBar
+        applyFilters(filteredWorks);
+    };
 
-  return (
-    <div class="retangulo">
-      {isShowingFilters && (
-        <div className="filter">
-        <br-list title="" data-toggle="true" density="small">
-            <br-item title="Definições" style={{background: "#F8F8F8", borderRadius: '8px', marginBottom: '8px'}}>
-            
-                <br-list style={{background: "#F8F8F8", padding: '12px'}}>
-                    <br-switch style={{background: "#F8F8F8", paddingTop: '8px'}} label="Acesso Aberto" id label-checked="" label-not-checked=""></br-switch>
-                    <br-switch style={{background: "#F8F8F8", paddingTop: '8px'}}label="Produção Nacional" id label-checked="" label-not-checked=""></br-switch>
-                    <br-switch style={{background: "#F8F8F8", paddingTop: '8px'}}label="Revisão por Pares" id label-checked="" label-not-checked=""></br-switch>
-                </br-list>
-                
-            </br-item>
-           
-            <br-item title="Ano de Publicação" style={{background: "#F8F8F8", borderRadius: '8px', marginBottom: '8px'}}>
-                <br-list style={{background: "#F8F8F8", padding: '12px'}}>
-                    <br-switch style={{background: "#F8F8F8", paddingTop: '8px'}}label="Acesso Aberto" id label-checked="" label-not-checked=""></br-switch>
-                    <br-switch style={{background: "#F8F8F8", paddingTop: '8px'}}label="Produção Nacional" id label-checked="" label-not-checked=""></br-switch>
-                    <br-switch style={{background: "#F8F8F8", paddingTop: '8px'}}label="Revisão por Pares" id label-checked="" label-not-checked=""></br-switch>
-                </br-list>
-            </br-item>
-            
-            <br-item title="Áreas" style={{background: "#F8F8F8", borderRadius: '8px', marginBottom: '8px'}}>
-                <br-list style={{background: "#F8F8F8", padding: '12px'}}>
-                    <br-input icon-sign="search" label="Label / Rótulo" placeholder="Busca"></br-input>
-                    <br-checkbox style={{background: "#F8F8F8", paddingTop: '8px'}}
-                        label="Ciências Humanas"
-                        name="base"
-                        aria-label=""
-                        model="check1"
-                    ></br-checkbox>
-                    <br-checkbox style={{background: "#F8F8F8", paddingTop: '8px'}}
-                        label="Ciências Exatas"
-                        name="base"
-                        aria-label=""
-                        model="check1"
-                    ></br-checkbox>
-                    <br-checkbox style={{background: "#F8F8F8", paddingTop: '8px'}}
-                        label="Ciências Biológicas"
-                        name="base"
-                        aria-label=""
-                        model="check1"
-                    ></br-checkbox>
+    return (
+        <div class="retangulo">
+            {isShowingFilters && (
+                <div className="filter">
+                    <br-list title="" data-toggle="true" density="small">
+                        <br-item title="Definições" style={{ background: "#F8F8F8", borderRadius: '8px', marginBottom: '8px' }}>
 
-                </br-list>
-            </br-item>
-        </br-list>
+                            <br-list style={{ background: "#F8F8F8", marginLeft: '12px', marginBottom: '12px', marginRight: '12px'}}>
+                                <br-switch style={{ background: "#F8F8F8"}} label="Acesso Aberto" id label-checked="" label-not-checked=""></br-switch>
+                                <br-switch style={{ background: "#F8F8F8"}} label="Produção Nacional" id label-checked="" label-not-checked=""></br-switch>
+                                <br-switch style={{ background: "#F8F8F8"}} label="Revisão por Pares" id label-checked="" label-not-checked=""></br-switch>
+                            </br-list>
 
-        {/*
+                        </br-item>
+
+                        <br-item title="Ano de Publicação" style={{ background: "#F8F8F8", borderRadius: '8px', marginBottom: '8px' }}>
+                            <br-list style={{ background: "#F8F8F8", marginLeft: '12px', marginBottom: '12px', marginRight: '12px'}}>
+                                <br-input
+                                icon="calendar"
+                                    label="De"
+                                    placeholder="1970"
+                                    density="small"
+                                ></br-input>
+                                <br-input
+                                icon="calendar"
+                                    label="Até"
+                                    placeholder="2024"
+                                    density="small"
+                                ></br-input>
+
+                            </br-list>
+                        </br-item>
+
+                        <br-item title="Áreas" style={{ background: "#F8F8F8", borderRadius: '8px', marginBottom: '8px' }}>
+                            <br-list style={{ background: "#F8F8F8", marginLeft: '12px', marginBottom: '12px', marginRight: '12px'}}>
+                                <br-input icon-sign="search" label="" placeholder="Busca"></br-input>
+                                <br-checkbox style={{ background: "#F8F8F8", paddingTop: '8px'}}
+                                    label="Ciências Humanas"
+                                    name="base"
+                                    aria-label=""
+                                    model="check1"
+                                ></br-checkbox>
+                                <br-checkbox style={{ background: "#F8F8F8", paddingTop: '8px' }}
+                                    label="Ciências Exatas"
+                                    name="base"
+                                    aria-label=""
+                                    model="check1"
+                                ></br-checkbox>
+                                <br-checkbox style={{ background: "#F8F8F8", paddingTop: '8px' }}
+                                    label="Ciências Biológicas"
+                                    name="base"
+                                    aria-label=""
+                                    model="check1"
+                                ></br-checkbox>
+                                <br-button>Mostrar mais</br-button>
+                            </br-list>
+                        </br-item>
+                    </br-list>
+
+                    <br-button style={{width: "220px"}} type="primary">Filtrar</br-button>
+
+                    {/*
           <div>
             <strong>Acesso Aberto</strong>
           </div>
@@ -338,10 +350,10 @@ function FilterBar({
             </div>
           </div>
            */}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default FilterBar;
